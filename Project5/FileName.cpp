dd058b2dee2279289a276e58e42bf8b316c9bd52
Gerald <Gerald@GARY>
2025-01-30 14:01:42 -0600

    Add project files.

---
+++ Project5/FileName.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+  
+    int A = pow(6, 2) ;
+    int B = pow(6, 2) ;
+
+    cout << A<< endl;
+    cout << B <<endl;
+
+    cout << A + B << endl;
+
+    cout << sqrt(A + B)<< endl;
+
+    string tweet; 
+   getline(cin, tweet);
+        cout << "you said:" << tweet << endl;
+        int tweetLength = tweet.length();
+       
+        cout << "Your tweet is " << tweetLength << " characters long." << endl;
+  
+         
+    
+    return 0;
+}
+
